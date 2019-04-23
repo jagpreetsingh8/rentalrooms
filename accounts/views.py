97d0dd3e24fc319f05b9aaa51871527a02603cd4
jagpreetsingh8 <jagpreetjaura8@gmail.com>
2019-04-23 13:53:05 +0530

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render ,redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == "POST":
-        messages.error(request, 'Testing error message')    
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2'] 
+
+        #check if passwords match
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'that username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'that email is taken')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username,password=password,
+                    email=email,first_name=first_name,last_name=last_name)
+                    #login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'you are now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'you are now registered and can log in')
+                    return redirect('login')
+                    # end of nested if
+     
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
